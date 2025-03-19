d7a98745dcc5ca3573f9ff87a8a3af74390b785b
Charlie Birks <charlie@daft.games>
2025-03-19 23:35:12 +0000

    PICOLIB: file IO
    Now we get as far as trying to cache a mix, which fails for obvious reasons

--- PICOLIB/file.cpp
+++ PICOLIB/file.cpp
@@ -2,39 +2,83 @@
 
 #include "file.h"
 
+#include "fatfs/ff.h"
+
 void *IO_Open_File(const char *filename, int mode)
 {
-    printf("open %s\n", filename);
-    return NULL;
+    int ff_mode;
+
+    if(mode == READ)
+        ff_mode = FA_READ;
+    else if(mode == WRITE)
+        ff_mode = FA_CREATE_ALWAYS | FA_WRITE;
+    else if(mode == (READ | WRITE))
+        ff_mode = FA_CREATE_ALWAYS | FA_READ | FA_WRITE;
+    else
+        return NULL;
+
+    auto fp = new FIL;
+
+    if(f_open(fp, filename, ff_mode) != FR_OK)
+    {
+        delete fp;
+        return NULL;
+    }
+
+    return fp;
 }
 
 void IO_Close_File(void *handle)
 {
+    auto fp = (FIL *)handle;
+    f_close(fp);
+    delete fp;
 }
 
 bool IO_Read_File(void *handle, void *buffer, size_t count, size_t &actual_read)
 {
-    return false;
+    auto fp = (FIL *)handle;
+    UINT read;
+    if(f_read(fp, buffer, count, &read) != FR_OK)
+        return false;
+
+    actual_read = read;
+    return true;
 }
 
 bool IO_Write_File(void *handle, const void *buffer, size_t count, size_t &actual_written)
 {
-    return false;
+    auto fp = (FIL *)handle;
+    UINT written;
+    if(f_write(fp, buffer, count, &written) != FR_OK)
+        return false;
+
+    actual_written = written;
+    return true;
 }
 
 size_t IO_Seek_File(void *handle, size_t offset, int origin)
 {
-    return 0;
+    auto fp = (FIL *)handle;
+    if(origin == SEEK_SET)
+        f_lseek(fp, offset);
+    else if(origin == SEEK_CUR)
+        f_lseek(fp, f_tell(fp) + offset);
+    else if(origin == SEEK_END)
+        f_lseek(fp, f_size(fp) + offset);
+       
+    return f_tell(fp);
 }
 
 size_t IO_Get_File_Size(void *handle)
 {
-    return 0;
+    auto fp = (FIL *)handle;
+    return f_size(fp);
 }
 
 bool IO_Delete_File(const char *filename)
 {
-    return false;
+    return f_unlink(filename) == FR_OK;
 }
 
 bool Find_First_File(const char *path_glob, FindFileState &state)
@@ -58,5 +102,10 @@ void End_Find_File(FindFileState &state)
 
 uint64_t Disk_Space_Available()
 {
-    return 1024 * 1024 * 1024;
+    DWORD free_clusters = 0;
+    FATFS *fs;
+    if(f_getfree("", &free_clusters, &fs) != FR_OK)
+        return 0;
+
+    return uint64_t(free_clusters) * fs->csize * 512;
 }
--- PICOLIB/init.cpp
+++ PICOLIB/init.cpp
@@ -1,10 +1,15 @@
 #include "pico/stdlib.h"
 
 #include "storage.h"
+#include "fatfs/ff.h"
+
+static FATFS fs;
 
 void Pico_Init()
 {
     stdio_init_all();
 
     storage_init();
+    f_mount(&fs, "", 0);
+    f_chdir("/CnC/");
 }
