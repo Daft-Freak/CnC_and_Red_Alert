d529865dda3d20c2574ab11346ccf89b568c404a
Charlie Birks <charlie@daft.games>
2025-03-19 20:15:49 +0000

    Shrink all the filename buffers
    512 bytes was always too much

--- port/ex_string.h
+++ port/ex_string.h
@@ -3,8 +3,9 @@
 
 // paths
 #define _MAX_PATH 260
-#define _MAX_FNAME 256
-#define _MAX_EXT 256
+// most of the uses of these in the game are expecting dos filenames
+#define _MAX_FNAME 9 // 8 + null
+#define _MAX_EXT 5 // 3 + leading . + null
 #define _MAX_DRIVE 3
 void _splitpath(const char *path, char *drive, char *dir, char *fname, char *ext);
 void _makepath(char *path, const char *drive, const char *dir, const char *fname, const char *ext);
