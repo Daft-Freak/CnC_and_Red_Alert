40bec8829d3583d26c50cf1149c95135ec1bcd5f
Charlie Birks <charlie@daft.games>
2025-03-20 13:27:06 +0000

    PICOLIB: cache mix files in flash
    These are megabytes of data, but fortunately read only

--- PICOLIB/init.cpp
+++ PICOLIB/init.cpp
@@ -3,13 +3,16 @@
 #include "storage.h"
 #include "fatfs/ff.h"
 
+#include "picolib.h"
+
 static FATFS fs;
 
 void Pico_Init()
 {
     stdio_init_all();
 
-    storage_init();
     f_mount(&fs, "", 0);
     f_chdir("/CnC/");
+
+    Pico_Flash_Cache_Init();
 }
