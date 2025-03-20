3226bb3a8c69654a90f626d9fdd545b3dafd3c9b
Charlie Birks <charlie@daft.games>
2025-03-20 17:08:32 +0000

    PICOLIB: PSRAM init code

--- PICOLIB/init.cpp
+++ PICOLIB/init.cpp
@@ -1,15 +1,29 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 
-#include "storage.h"
+#include "psram.h"
 #include "fatfs/ff.h"
 
 #include "picolib.h"
 
 static FATFS fs;
 
+#ifdef PIMORONI_PICO_PLUS2_RP2350
+#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
+#elif defined(PIMORONI_PICO_PLUS2_W_RP2350)
+#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_W_PSRAM_CS_PIN
+#else
+#error "No PSRAM CS!"
+#endif
+
 void Pico_Init()
 {
+
     stdio_init_all();
+
+    size_t psramSize = psram_init(PSRAM_CS_PIN);
+    printf("detected %i bytes PSRAM\n", psramSize);
 
     f_mount(&fs, "", 0);
     f_chdir("/CnC/");
