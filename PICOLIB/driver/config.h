1f876ee34e626cd7a8c480e8ff4a810abf3474e7
Charlie Birks <charlie@daft.games>
2025-03-21 20:04:27 +0000

    PICOLIB: import DBI display code from 32blit
    With palette patches... and non-palette removed

--- PICOLIB/driver/config.h
+++ PICOLIB/driver/config.h
@@ -4,3 +4,16 @@
 #define SD_MOSI 11
 #define SD_MISO 8
 #define SD_CS   9
+
+#define DISPLAY_WIDTH  320
+#define DISPLAY_HEIGHT 240
+
+#define LCD_DC_PIN        16
+#define LCD_CS_PIN        17
+#define LCD_SCK_PIN       18
+#define LCD_MOSI_PIN      19
+#define LCD_BACKLIGHT_PIN 20
+#define LCD_VSYNC_PIN     21
+
+#define LCD_ROTATION 90
+#define LCD_MAX_CLOCK 62500000
