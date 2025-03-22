189c7217ed6f92c6df52a6fc03c055975a36b885
Charlie Birks <charlie@daft.games>
2025-03-22 16:53:16 +0000

    PICOLIB: some intial keyboard/mouse button handling
    Not tracking state so modifiers don't work

--- PICOLIB/init.cpp
+++ PICOLIB/init.cpp
@@ -7,6 +7,7 @@
 #include "psram.h"
 #include "fatfs/ff.h"
 
+#include "keyboard.h"
 #include "mouse.h"
 #include "picolib.h"
 
@@ -21,11 +22,14 @@ static FATFS fs;
 #endif
 
 static uint16_t mouse_x = 0, mouse_y = 0;
+static uint8_t mouse_buttons = 0;
+extern WWKeyboardClass *TheKeyboard;
 
 // input glue
 void update_key_state(uint8_t code, bool state)
 {
-    
+    if(TheKeyboard)
+        TheKeyboard->Put_Key_Message(code, !state);
 }
 
 void update_mouse_state(int8_t x, int8_t y, bool left, bool right)
@@ -36,7 +40,28 @@ void update_mouse_state(int8_t x, int8_t y, bool left, bool right)
     mouse_y = tmp_y < 0 ? 0 : (tmp_y > 199 ? 199 : tmp_y);
 
     Update_Mouse_Pos(mouse_x, mouse_y);
-    // also need to forward the event to the keyboard
+
+    // also need to forward the buttons to the keyboard
+    if(TheKeyboard)
+    {
+        bool old_left = mouse_buttons & 1;
+        bool old_right = mouse_buttons & 2;
+
+        if(old_left != left)
+        {
+            TheKeyboard->Put_Key_Message(VK_LBUTTON, !left);
+            TheKeyboard->Put(mouse_x);
+            TheKeyboard->Put(mouse_y);
+        }
+        if(old_right != right)
+        {
+            TheKeyboard->Put_Key_Message(VK_RBUTTON, !right);
+            TheKeyboard->Put(mouse_x);
+            TheKeyboard->Put(mouse_y);
+        }
+
+        mouse_buttons = (left ? 1 : 0) | (right ? 2 : 0);
+    }
 }
 
 void Pico_Init()
