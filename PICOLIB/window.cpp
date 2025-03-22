7d6489839412313f5d65f6917b37b56f3c5fc7dd
Charlie Birks <charlie@daft.games>
2025-03-22 15:49:03 +0000

    PICOLIB: start connecting up mouse
    Can move but not click (because that goes through the keyboard code, obviously)

--- PICOLIB/window.cpp
+++ PICOLIB/window.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "tusb.h"
+
 #include "ww_win.h"
 #include "net_select.h"
 #include "gbuffer.h"
@@ -23,7 +25,9 @@ void SDL_Event_Loop()
 {
 
     // this is replacing WSAAsyncSelect, which would send through the windows event loop
-    Socket_Select();
+    //Socket_Select();
+
+    tuh_task();
 }
 
 void SDL_Send_Quit()
