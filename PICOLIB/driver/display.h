ad560a65e78309bce05ebdc2e79be9430544dc78
Charlie Birks <charlie@daft.games>
2025-03-24 11:22:25 +0000

    PICOLIB: make an attempt at syncing display
    At least try to not start a copy to the framebuffer when we're still reading it

--- PICOLIB/driver/display.h
+++ PICOLIB/driver/display.h
@@ -5,4 +5,6 @@ void init_display();
 void update_display(uint32_t time);
 void set_screen_palette(const uint8_t *colours, int num_cols);
 
+bool display_render_needed();
+
 uint8_t *get_framebuffer();
