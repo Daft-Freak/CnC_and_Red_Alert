6cd97dbeefd6ff3c546e163978dd3a3a13c57d23
Charlie Birks <charlie@daft.games>
2025-03-29 18:20:57 +0000

    SDLLIB: avoid accessing non-existing strings in Extract_String
    Better than crashing becuase you opened the modem settings without EXPAND.MIX

--- SDLLIB/dipthong.cpp
+++ SDLLIB/dipthong.cpp
@@ -10,5 +10,13 @@ char *Extract_String(void const *data, int string)
 	if (!data || string < 0) return(NULL);
 		
 	ptr = (unsigned short int const *)data;
+
+	// assume offset of first string is end of index table
+	int numstrings = ptr[0] / 2;
+
+	// don't index past the end (might happen if expansion files missing)
+	if(string >= numstrings)
+		return NULL;
+
 	return (((char*)data) + ptr[string]);
 }
