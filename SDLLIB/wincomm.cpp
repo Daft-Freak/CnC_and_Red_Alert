f1fe552ea7607ce9988dc73902e447be0b6e194c
Charlie Birks <charlie@daft.games>
2025-03-30 12:56:32 +0100

    SDLLIB: optional serial port support using libserialport

--- SDLLIB/wincomm.cpp
+++ SDLLIB/wincomm.cpp
@@ -1,12 +1,18 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef LIBSERIALPORT
+#include <libserialport.h>
+#endif
 
 #include "wincomm.h"
 #include "modemreg.h"
 
-WinModemClass *SerialPort;
+WinModemClass *SerialPort = NULL;
 
-WinModemClass::WinModemClass(void)
+WinModemClass::WinModemClass(void) : PortHandle(NULL)
 {
 
 }
@@ -18,24 +24,115 @@ WinModemClass::~WinModemClass(void)
 
 HANDLE WinModemClass::Serial_Port_Open(char *device_name, int baud, int parity, int wordlen, int stopbits, int flowcontrol)
 {
-    printf("WinModemClass::%s\n", __func__);
+#ifdef LIBSERIALPORT
+    sp_port *port;
+    sp_return result = sp_get_port_by_name(device_name, &port);
+
+    if(result != SP_OK)
+        return NULL;
+
+    // open
+    result = sp_open(port, SP_MODE_READ_WRITE);
+    if(result != SP_OK)
+    {
+        sp_free_port(port);
+        return NULL;
+    }
+
+    // configure
+    result = sp_set_baudrate(port, baud);
+    if(result != SP_OK)
+    {
+        sp_close(port);
+        sp_free_port(port);
+        return NULL;
+    }
+
+    sp_parity libsp_parity = SP_PARITY_NONE;
+    switch(parity)
+    {
+        case 'O': libsp_parity = SP_PARITY_ODD; break;
+        case 'E': libsp_parity = SP_PARITY_EVEN; break;
+        case 'N': libsp_parity = SP_PARITY_NONE; break;
+        case 'S': libsp_parity = SP_PARITY_SPACE; break;
+        case 'M': libsp_parity = SP_PARITY_MARK; break;
+    }
+
+    result = sp_set_parity(port, libsp_parity);
+    if(result != SP_OK)
+    {
+        sp_close(port);
+        sp_free_port(port);
+        return NULL;
+    }
+
+    result = sp_set_bits(port, wordlen);
+    if(result != SP_OK)
+    {
+        sp_close(port);
+        sp_free_port(port);
+        return NULL;
+    }
+
+    result = sp_set_stopbits(port, stopbits);
+    if(result != SP_OK)
+    {
+        sp_close(port);
+        sp_free_port(port);
+        return NULL;
+    }
+
+    result = sp_set_flowcontrol(port, flowcontrol ? SP_FLOWCONTROL_RTSCTS : SP_FLOWCONTROL_NONE);
+    if(result != SP_OK)
+    {
+        sp_close(port);
+        sp_free_port(port);
+        return NULL;
+    }
+
+    PortHandle = port;
+    return PortHandle;
+#else
+
+    printf("WinModemClass::%s : no serial port support\n", __func__);
     return NULL;
+#endif
 }
 
 void WinModemClass::Serial_Port_Close(void)
 {
-    printf("WinModemClass::%s\n", __func__);
+#ifdef LIBSERIALPORT
+    if(PortHandle)
+    {
+        sp_port *port = (sp_port *)PortHandle;
+        sp_close(port);
+        sp_free_port(port);
+        PortHandle = NULL;
+    }
+#endif
 }
 
 int	WinModemClass::Read_From_Serial_Port(unsigned char *dest_ptr, int buffer_len)
 {
-    printf("WinModemClass::%s\n", __func__);
+#ifdef LIBSERIALPORT
+    if(PortHandle)
+    {
+        sp_return result = sp_nonblocking_read((sp_port *)PortHandle, dest_ptr, buffer_len);
+
+        if(result > 0) {
+            return result;
+        }
+    }
+#endif
     return 0;
 }
 
 void WinModemClass::Write_To_Serial_Port(unsigned char *buffer, int length)
 {
-    printf("WinModemClass::%s\n", __func__);
+#ifdef LIBSERIALPORT
+    if(PortHandle)
+        sp_blocking_write((sp_port *)PortHandle, buffer, length, 0);
+#endif
 }
 
 void WinModemClass::Set_Modem_Dial_Type(WinCommDialMethodType method)
@@ -83,17 +180,35 @@ void WinModemClass::Set_Abort_Function(int (*func)(void))
 
 HANDLE WinModemClass::Get_Port_Handle(void)
 {
-    printf("WinModemClass::%s\n", __func__);
-    return NULL;
+    return PortHandle;
 }
 
 ModemRegistryEntryClass::ModemRegistryEntryClass(int modem_number) :
     ModemName(NULL), ModemDeviceName(NULL), ErrorCorrectionEnable(NULL), ErrorCorrectionDisable(NULL), CompressionEnable(NULL),
     CompressionDisable(NULL), HardwareFlowControl(NULL), NoFlowControl(NULL)
 {
+#ifdef LIBSERIALPORT
+    struct sp_port **port_list;
+
+    sp_return result = sp_list_ports(&port_list);
+
+    if(result == SP_OK)
+    {
+        for(int i = 0; port_list[i]; i++)
+        {
+            if(i == modem_number)
+            {
+                ModemName = ModemDeviceName = strdup(sp_get_port_name(port_list[i]));
+                break;
+            }
+        }
+
+        sp_free_port_list(port_list);
+    }
+#endif
 }
 
 ModemRegistryEntryClass::~ModemRegistryEntryClass()
 {
-
+    if(ModemName) free(ModemName);
 }
