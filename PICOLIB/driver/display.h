64191473b633175145f629a64e0dbae8007d25da
Charlie Birks <charlie@daft.games>
2025-04-11 18:53:07 +0100

    PICOLIB: ... add the display cursor decls...
    Whoops

--- PICOLIB/driver/display.h
+++ PICOLIB/driver/display.h
@@ -10,3 +10,6 @@ void set_screen_palette(const uint8_t *colours, int num_cols);
 bool display_render_needed();
 
 uint8_t *get_framebuffer();
+
+void display_set_cursor(uint8_t *data, int w, int h);
+void display_set_cursor_pos(int x, int y);
