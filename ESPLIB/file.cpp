eda51935cedcad9a78daabece3f0149ac1e38cbe
Charlie Birks <charlie@daft.games>
2025-08-27 16:30:49 +0100

    ESPLIB: handle some file IO
    Mostly copied from SDLLIB/PICOLIB...

--- ESPLIB/file.cpp
+++ ESPLIB/file.cpp
@@ -1,42 +1,71 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include "esp_vfs_fat.h"
 
 #include "file.h"
 
-// FIXME: VFS + std C api
-
 void *IO_Open_File(const char *filename, int mode)
 {
-    return nullptr;
+    const char *mode_str;
+
+    if(mode == READ)
+        mode_str = "rb";
+    else if(mode == WRITE)
+        mode_str = "wb";
+    else if(mode == (READ | WRITE))
+        mode_str = "w+b";
+    else
+        return NULL;
+
+    return fopen(filename, mode_str);
 }
 
 void IO_Close_File(void *handle)
 {
+    auto file = (FILE *)handle;
+    fclose(file);
 }
 
 bool IO_Read_File(void *handle, void *buffer, size_t count, size_t &actual_read)
 {
-    return false;
+    auto file = (FILE *)handle;
+    actual_read = fread(buffer, 1, count, file);
+    return ferror(file) == 0;
 }
 
 bool IO_Write_File(void *handle, const void *buffer, size_t count, size_t &actual_written)
 {
-    return false;
+    auto file = (FILE *)handle;
+    actual_written = fwrite(buffer, 1, count, file);
+    return ferror(file) == 0;
 }
 
 size_t IO_Seek_File(void *handle, size_t offset, int origin)
 {
-    return 0;
+    auto file = (FILE *)handle;
+    fseek(file, offset, origin);
+    return ftell(file);
 }
 
 size_t IO_Get_File_Size(void *handle)
 {
-    return 0;
+    auto file = (FILE *)handle;
+    long pos = ftell(file);
+
+    fseek(file, 0, SEEK_END);
+
+    long length = ftell(file);
+
+    fseek(file, pos, SEEK_SET);
+
+    return length;
 }
 
 bool IO_Delete_File(const char *filename)
 {
-    return false;
+    return unlink(filename) == 0;
 }
 
 
@@ -57,5 +86,12 @@ void End_Find_File(FindFileState &state)
 
 uint64_t Disk_Space_Available()
 {
-    return 0;
+    // TODO: is there a generic way of doing this?
+
+    DWORD free_clusters = 0;
+    FATFS *fs;
+    if(f_getfree("", &free_clusters, &fs) != FR_OK)
+        return 0;
+
+    return uint64_t(free_clusters) * fs->csize * 512;
 }
