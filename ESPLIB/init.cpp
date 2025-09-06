d0429af1318a4f6f28088c4d010e91a67a16451c
Charlie Birks <charlie@daft.games>
2025-09-06 15:19:18 +0100

    ESPLIB: wire up networking
    This is mostly deleting code

--- ESPLIB/init.cpp
+++ ESPLIB/init.cpp
@@ -258,7 +258,9 @@ void Pico_Wifi_Init(const char *ssid, const char *pass)
     ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    auto netif = esp_netif_create_default_wifi_sta();
+
+    esp_netif_set_hostname(netif, "esp32");
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -293,7 +295,7 @@ void Pico_Wifi_Init(const char *ssid, const char *pass)
     if (bits & WIFI_CONNECTED_BIT)
         printf("Connected to SSID:%s password:%s\n", ssid, pass);
     else if (bits & WIFI_FAIL_BIT)
-        printf("Failed to connect to SSID:%s password:%s\n", ssid, pass); 
+        printf("Failed to connect to SSID:%s password:%s\n", ssid, pass);
 }
 
 // app_main -> main wrapper
