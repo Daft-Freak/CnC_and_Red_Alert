72ab1e1c03c4acd6223acc5a83591e0b10e1d2ad
speedwago <speedwago@gmail.com>
2025-12-17 20:30:17 +0100

    fixes compiler error with microsoft build tools

--- TD/ipxstub.cpp
+++ TD/ipxstub.cpp
@@ -1,51 +1,56 @@
 #include "ipx95.h"
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#define stdcall __stdcall
+#else
+#define stdcall
+#endif
 
-bool IPX_Initialise(void)
+bool stdcall IPX_Initialise(void)
 {
     return false;
 }
 
-bool IPX_Get_Outstanding_Buffer95(unsigned char *buffer)
+bool stdcall IPX_Get_Outstanding_Buffer95(unsigned char *buffer)
 {
     return false;
 }
 
-void IPX_Shut_Down95(void)
+void stdcall IPX_Shut_Down95(void)
 {
 
 }
 
-int  IPX_Send_Packet95(unsigned char *, unsigned char *, int, unsigned char*, unsigned char*)
-{
-    return 0;
-}
-
-int  IPX_Broadcast_Packet95(unsigned char *, int)
+int stdcall IPX_Send_Packet95(unsigned char *, unsigned char *, int, unsigned char*, unsigned char*)
 {
     return 0;
 }
 
-bool IPX_Start_Listening95(void)
+int stdcall IPX_Broadcast_Packet95(unsigned char *, int)
+{
+    return 0;
+}
+
+bool stdcall IPX_Start_Listening95(void)
 {
     return false;
 }
 
-int  IPX_Open_Socket95(int socket)
+int stdcall IPX_Open_Socket95(int socket)
 {
     return 0;
 }
 
-void IPX_Close_Socket95(int socket)
+void stdcall IPX_Close_Socket95(int socket)
 {
 
 }
 
-int  IPX_Get_Connection_Number95(void)
+int stdcall IPX_Get_Connection_Number95(void)
 {
     return 0;
 }
 
-int  IPX_Get_Local_Target95(unsigned char *, unsigned char*, unsigned short, unsigned char*)
+int stdcall IPX_Get_Local_Target95(unsigned char *, unsigned char*, unsigned short, unsigned char*)
 {
     return 0;
 }
